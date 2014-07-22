0eb02a190fd5d63e71c7f81a8152749c85218a8d
Simon J Hogan <simon.hogan@iinet.net.au>
2014-07-22 20:25:54 +1000

    Updated copyright notice in script files

--- WebContent/system/script/player.js
+++ WebContent/system/script/player.js
@@ -1,8 +1,17 @@
-﻿/*
-** PLEX for LG Media Center
-** Created by Simon J. Hogan
-** Copyright 2013 h3consulting
-** http://plex.h3consulting.net
+﻿/**
+PLEX for LG Media Center
+
+Copyright 2014 Simon J. Hogan (Sith'ari Consulting)
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this 
+file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under 
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+either express or implied. See the License for the specific language governing permissions 
+and limitations under the License.
 **/
 
 function Player() {		
